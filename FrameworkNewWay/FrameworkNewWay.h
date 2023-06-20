a915d6cdf40fd03e8bf2739188528d58b04d4a37
Shubham Deshmukh <shubhamdeshmukh69@gmail.com>
2023-06-20 19:36:14 +0530

    Initial Commit

---
+++ FrameworkNewWay/FrameworkNewWay.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkNewWay.h
+//  FrameworkNewWay
+//
+//  Created by Shubham Deshmukh on 20/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkNewWay.
+FOUNDATION_EXPORT double FrameworkNewWayVersionNumber;
+
+//! Project version string for FrameworkNewWay.
+FOUNDATION_EXPORT const unsigned char FrameworkNewWayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkNewWay/PublicHeader.h>
+
+
